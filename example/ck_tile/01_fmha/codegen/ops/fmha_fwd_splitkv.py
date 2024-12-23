bb0934704e70a4ea69d91f53b1650aaf5cac87e0
Po Yen Chen <PoYen.Chen@amd.com>
2024-12-23 14:31:08 -0600

    Only generate V rowmajor kernels

--- example/ck_tile/01_fmha/codegen/ops/fmha_fwd_splitkv.py
+++ example/ck_tile/01_fmha/codegen/ops/fmha_fwd_splitkv.py
@@ -734,6 +734,7 @@ def get_fwd_splitkv_blobs(kernel_filter : Optional[str], receipt, mask_impl) -> 
                         continue
                 if receipt == 2:
                     cond = dtype in ['fp16', 'bf16']
+                    cond &= pipeline.F_vlayout == 'row'
                     cond &= pipeline.F_bias in ['no', 'alibi']
                     cond &= pipeline.F_squant == 'f'
                     if not cond:
