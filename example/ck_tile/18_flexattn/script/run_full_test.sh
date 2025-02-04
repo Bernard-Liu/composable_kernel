a671c408e3f4bd125ac4577e536f29041b9a52fb
Aviral Goel <aviral.goel@amd.com>
2025-02-04 19:59:05 +0000

    print error message

--- example/ck_tile/18_flexattn/script/run_full_test.sh
+++ example/ck_tile/18_flexattn/script/run_full_test.sh
@@ -37,6 +37,7 @@ example/ck_tile/18_flexattn/script/smoke_test_fwd.sh
 #run performance benchmarks
 export fmha_fwd_log="perf_tile_flex_attn_$GPU_arch.log"
 print_log_header $fmha_fwd_log $env_type $branch $host_name
+echo "Running performance benchmark for tile_flex_attn_fwd"
 example/ck_tile/18_flexattn/script/benchmark_fwd.sh 2>&1 | tee -a $fmha_fwd_log
 
 
